504df54b3dcd432ae0b13d16e7a8768e1c565b16
SaillisA <dumaine.lisa04@gmail.com>
2023-11-27 15:54:50 +0100

    Avancer

--- Assets/Script/Joueur.cs
+++ Assets/Script/Joueur.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Joueur : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject joueur;
+    public GameObject cam;
+    public Image cameraJoueur;
+
+    public GameObject imageboutontest;
+
+
 
     void Start()
     {
@@ -16,9 +23,48 @@ public class Joueur : MonoBehaviour
     void Update()
     {
         
-        if(Input.GetKey(KeyCode.RightArrow))
-        {
-            joueur.gameObject.transform.position = joueur.gameObject.transform.position + Vector3.right * 3;
-        }
+        
+    }
+
+
+
+
+    //Fonctions pour les boutons de deplacements
+
+    public void JoueurDroite()
+    {
+        joueur.gameObject.transform.position = joueur.gameObject.transform.position + Vector3.right * 0.07f;
+    }
+    public void JoueurGauche()
+    {
+        joueur.gameObject.transform.position = joueur.gameObject.transform.position + Vector3.left * 0.07f;
+    }
+    public void JoueurAvant()
+    {
+        joueur.gameObject.transform.position = joueur.gameObject.transform.position + Vector3.forward * 0.07f;
+    }
+    public void JoueurDerriere()
+    {
+        joueur.gameObject.transform.position = joueur.gameObject.transform.position + Vector3.back * 0.07f;
+    }
+
+    //fonctions pour les boutons de camera
+
+    public void CameraHaut()
+    {
+        cameraJoueur.gameObject.transform.Rotate(-1, 0, 0);  
+    }
+    public void CameraDroite()
+    {
+        joueur.gameObject.transform.Rotate(0,1,0);
+    }
+    //joueur.gameObject.transform.rotation
+    public void CameraGauche()
+    {
+        joueur.gameObject.transform.Rotate(0,-1,0); 
+    }
+    public void CameraBas()
+    {
+        cameraJoueur.gameObject.transform.Rotate(1,0,0);
     }
 }
