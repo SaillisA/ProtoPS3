350abcae9ab1d581dc84b5d6fa90127652ce6478
SaillisA <dumaine.lisa04@gmail.com>
2023-11-27 13:21:03 +0100

    test

---
+++ Assets/Script/Joueur.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joueur : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject joueur;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
